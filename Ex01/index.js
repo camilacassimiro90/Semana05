6c7544286aa4ce1cd9bffa11dc5214bbd5be1ba0
Cami <camilacassimiro@gmail.com>
2022-03-29 15:37:00 -0300

    enviei exercício 01

---
+++ Ex01/index.js
@@ -0,0 +1,6 @@
+// aqui vai importar
+// import * as saudacao from './saudacao.js' ->>> importar tudo o que tem no arquivo saudacao.js
+
+import { saudacao } from './saudacao.js'
+
+console.log(saudacao)
---
+++ Ex01/saudacao.js
@@ -0,0 +1,2 @@
+export const saudacao = msg => msg
+console.log(saudacao('Olá!'))
