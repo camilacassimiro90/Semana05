9065f11b85be0b5c1a97d420236ce76c5eb01da8
Cami <camilacassimiro@gmail.com>
2022-03-31 17:36:39 -0300

    adicionei exercício 06

---
+++ Ex06/index.js
@@ -0,0 +1,62 @@
+// Dentro da classe Pedido, adicione um método **adicionarProduto**, que recebe como argumento um objeto do tipo Produto ( _caso deseje_, você pode realizar uma validação utilizando o operador _instanceof_).
+
+// Este método deve adicionar (**push**) à propriedade listaProdutos o produto passado como argumento.
+
+class Produto {
+  nome
+  preco
+  emEstoque
+  quantidade
+
+  constructor(nome, preco, emEstoque, quantidade) {
+    this.nome = nome
+    this.preco = preco
+    this.emEstoque = emEstoque
+    this.quantidade = quantidade
+  }
+}
+
+class Pedidos {
+  numeroPedido
+  dataPedido = new Date().toLocaleDateString()
+  estaPago = false
+  listaProdutos = []
+  nomeCliente
+  constructor(numeroPedido, nomeCliente) {
+    this.numeroPedido = numeroPedido
+    this.nomeCliente = nomeCliente
+  }
+  // adicionar item no array
+  adicionarProdutos(...produto) {
+    this.listaProdutos.push(produto)
+  }
+
+  calcularTotal() {
+    let valorTotal = 0
+    this.listaProdutos.forEach(produto => {
+      valorTotal += produto.quantidade * produto.preco
+    })
+    return valorTotal
+  }
+}
+
+const pedido1 = new Pedidos(
+  1,
+  'Camila',
+  this.estaPago,
+  this.dataPedido,
+  this.listaProdutos
+)
+
+const pedido2 = new Pedidos(
+  2,
+  'João',
+  this.estaPago,
+  this.dataPedido,
+  this.listaProdutos
+)
+
+pedido1.adicionarProdutos('mouse')
+console.log(pedido1)
+pedido2.adicionarProdutos('teclado', 'monitor')
+console.log(pedido2)
