4c4c0ae10c801c23094c3f1289311c43e5f3aab0
Cami <camilacassimiro@gmail.com>
2022-04-04 14:43:31 -0300

    adicionei enunciados dos exercícios 08, 09 e 10

--- Ex09/index.mjs
+++ Ex09/index.mjs
@@ -1,3 +1,7 @@
+// Utilizando o setInterval, crie uma função que mostre o horário atual e atualize automaticamente a cada 2 segundos.
+
+// O horário deve ser mostrado no console, no formato HH:MM:SS.
+
 function horario() {
   const data = new Date()
 
